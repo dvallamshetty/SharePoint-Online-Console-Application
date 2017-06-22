ce675ed2a15c458dd40cac03302e17339d5956b6
DilipKumarVallamshetty <dilip_kumar_vallamshetty@fanniemae.com>
2017-06-22 16:48:26 -0400

    First working version

---
+++ SP.DemoApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.DemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri siteUri = new Uri("https://fnma.sharepoint.com/sites/CPM_Operations/");
+            string realm = TokenHelper.GetRealmFromTargetUrl(siteUri);
+
+            string accessToken = TokenHelper.GetAppOnlyAccessToken(TokenHelper.SharePointPrincipal,
+                                                                    siteUri.Authority, realm).AccessToken;
+
+            using (var clientContext = TokenHelper.GetClientContextWithAccessToken(siteUri.ToString(), accessToken))
+            {
+                Web currentWeb = clientContext.Web;
+                clientContext.Load(currentWeb, w => w.Title, w => w.Description);
+                clientContext.ExecuteQuery();
+                Console.WriteLine(currentWeb.Title);
+                Console.WriteLine(currentWeb.Description);
+
+                //WebCreationInformation wci = new WebCreationInformation();
+                //wci.Url = "shinyweb";
+                //wci.Title = "New shiny web";
+                //Web shinyNewWeb = currentWeb.Webs.Add(wci);
+
+                // Create the sub web
+                clientContext.ExecuteQuery();
+            }
+
+            Console.WriteLine("...");
+            Console.ReadLine();
+        }
+    }
+}
